e02b275e2f13c960234545b6d357a1801730c321
Fassio Blatter <fassio@specpose.com>
2025-03-05 19:14:33 +0100

    Using Android executable shared object called libmain.so

--- app/jni/src/Android.mk
+++ app/jni/src/Android.mk
@@ -6,14 +6,16 @@ LOCAL_MODULE := main
 
 # Add your application source files here...
 LOCAL_SRC_FILES := \
-    YourSourceHere.cpp
+    $(LOCAL_PATH)/../../libs/arm64-v8a/libmain.so
+#    YourSourceHere.cpp
 
-SDL_PATH := ../SDL  # SDL
+#SDL_PATH := ../SDL  # SDL
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include  # SDL
+#LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include  # SDL
 
-LOCAL_SHARED_LIBRARIES := SDL3
+#LOCAL_SHARED_LIBRARIES := SDL3
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid  # SDL
 
-include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_SHARED_LIBRARY)
+include $(PREBUILT_SHARED_LIBRARY)
